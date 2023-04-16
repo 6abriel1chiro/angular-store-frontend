7830c58bd3d053fecd827b66f05b32ab17b3918d
Ogreaggressive <nicolascari.rz@gmail.com>
2023-04-16 13:33:29 -0400

    aumentando seguridad a nuestro login

--- src/app/jwt-interceptor.interceptor.ts
+++ src/app/jwt-interceptor.interceptor.ts
@@ -10,6 +10,13 @@ import {CookieService} from "ngx-cookie-service";
 import {catchError} from "rxjs/operators";
 import {Router} from "@angular/router";
 
+import jwt_decode from 'jwt-decode';
+
+interface DecodedToken {
+  exp: number;
+  // Add any other properties that you're decoding from the token
+}
+
 @Injectable()
 export class JwtInterceptorInterceptor implements HttpInterceptor {
   constructor(private cookieService: CookieService, private router: Router) {
@@ -20,6 +27,14 @@ export class JwtInterceptorInterceptor implements HttpInterceptor {
     const token: string = this.cookieService.get('token');
     let req = request;
     if (token) {
+      // Check if the token is still valid and not expired
+      const decodedToken = jwt_decode(token) as DecodedToken;
+      const currentTime = Date.now() / 1000;
+      if (decodedToken.exp < currentTime) {
+        this.cookieService.delete('token');
+        this.router.navigateByUrl('/login');
+        return throwError('Token expired');
+      }
       req = request.clone({
         setHeaders: {
           authorization: `Bearer ${token}`
@@ -27,18 +42,14 @@ export class JwtInterceptorInterceptor implements HttpInterceptor {
       });
     }
 
-
     return next.handle(req).pipe(
       catchError((err: HttpErrorResponse) => {
-
         if (err.status === 401) {
+          this.cookieService.delete('token');
           this.router.navigateByUrl('/login');
         }
-
         return throwError(err);
-
       })
     );
-
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,29 +8,55 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   email:string="";
   password:string="";
+  errorMessage: string = '';
+  loginAttempts: number = 0; // variable para contar los intentos de inicio de sesión
+  timer: any; // variable para almacenar el temporizador
 
   constructor(public userService: UsersService, private router:Router, private CookieService: CookieService) { }
 
   ngOnInit(): void {
 
   }
-  login():void {
-    this.userService.login(this.email,this.password ).subscribe((data) => {
-      this.router.navigate(['']);
-      this.CookieService.set('token', data.token)
-    });
+
+  login(): void {
+    if (this.loginAttempts >= 4) {
+      // Si ya se han hecho 4 intentos, mostrar mensaje de error y esperar 30 segundos antes de permitir otro intento
+      this.errorMessage = "Demasiados intentos. Intente de nuevo en 30 segundos.";
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        this.loginAttempts = 0;
+        this.errorMessage = '';
+      }, 30000);
+      return;
+    }
+
+    this.userService.login(this.email, this.password).subscribe(
+      (data) => {
+        if (data.token) {
+          // Save the token in a cookie with Secure and HttpOnly flags
+          this.CookieService.set('token', data.token, undefined, '/', undefined, true, 'Strict');
+          this.router.navigate(['']);
+        } else {
+          // Display error message if the server did not return a token
+          this.errorMessage = "Authentication failed";
+          this.loginAttempts++;
+        }
+      },
+      (error) => {
+        // Display error message if there was an error during the authentication process
+        this.errorMessage = error.message;
+        this.loginAttempts++;
+      }
+    );
   }
 
-
-goRegister():void {
-  this.router.navigate(['/register']);
-
-
-}
-
+  goRegister():void {
+    this.router.navigate(['/register']);
+  }
 
 }
